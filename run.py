a8779316d18a8c3d45a9cd7f1e6f8201085e051f
Saishiro29 <oleynik-alexandr@mail.ru>
2025-09-03 19:51:51 +0500

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
+from app import app
 from waitress import serve
-from app import app  # замени your_application на имя твоего main файла Flask
 
-if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=10000)
